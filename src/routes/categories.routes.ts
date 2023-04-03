29e108775ba1c477647a8bfe2192d51ff7d72c90
Leonardo_Soares <leonardoc.soares08@gmail.com>
2023-04-03 02:05:42 -0300

    aula 2 de upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import multer from 'multer'
 
 const categoriesRoutes = Router()
+const upload = multer({ dest: "./tmp", })
+
 
 categoriesRoutes
-    .post("/categories", (request, response) => { 
-        return createCategoryController.handle(request, response) 
+    .post("/categories", (request, response) => {
+        return createCategoryController.handle(request, response)
     })
 
     .get("/categories", (request, response) => {
         return listCategoriesController.handle(request, response)
     })
 
+    .post("/categories/import", upload.single("file") ,(request, response) => {
+        const { file } = request;
+        console.log(file)
+        response.send()
+    })
 
 
 export { categoriesRoutes }
